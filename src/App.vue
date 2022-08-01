3485ab3299199ca0bcbd89dffe1100191d463a4f
Nathan A. Paludo <natspaludo@gmail.com>
2022-08-01 20:55:29 -0300

    First commit.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <v-app theme="dark">
+        <v-main>
+            <RouterView />
+        </v-main>
+    </v-app>
+</template>
+
+<style scoped lang="scss"></style>
